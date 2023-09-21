b744b0b39860843961fe92ac7269e34869165e2e
YamasouA <akiakiskyhand@gmail.com>
2023-09-22 00:20:58 +0900

    ファイル名の修正

---
+++ srcs/conf/ConfigParser.hpp
@@ -0,0 +1,106 @@
+#ifndef CONFIGPASER_HPP
+#define CONFIGPASER_HPP
+
+#include <fstream>
+#include <sstream>
+#include <stdexcept>
+#include <exception>
+#include "VirtualServer.hpp"
+#include "Location.hpp"
+#include <map>
+
+
+class ConfigParser {
+	public:
+		explicit ConfigParser(const std::string& strs);
+		explicit ConfigParser();
+		ConfigParser(const ConfigParser& src);
+		ConfigParser& operator=(const ConfigParser& rhs);
+		~ConfigParser();
+
+		void parseConf();
+		void skip();
+		void trim(std::string& str);
+		void expect(char c);
+		std::vector<VirtualServer> getServerConfs()const;
+		void fixUp();
+		void checkServer();
+		void checkLocation();
+		void set_buf(std::string strs);
+	private:
+		std::string buf;
+		std::vector<VirtualServer> serve_confs;
+		size_t idx;
+
+        static const int kRootExist = 1;
+        static const int kAliasExist = 2;
+        static const int kMaxSizeExist = 4;
+        static const int kMethodExist = 8;
+        static const int kAutoIndexExist = 16;
+        static const int kUploadPathExist = 32;
+        static const int kIndexExist = 64;
+        static const int kReturnExist = 128;
+        static const int kErrorPageExist = 256;
+        static const int kCgiExtExist = 512;
+
+		VirtualServer parseServe();
+		void setUriToMap(std::string prefix, std::string prefix_root, Location location, const VirtualServer& v_serv);
+		void uriToMap(VirtualServer& vServer);
+		Location parseLocation();
+		std::string getToken(char delimiter);
+		std::string getTokenToEOL();
+		std::map<std::string, Location> uri2location;
+
+		void handleListenInServ(VirtualServer& v_serv);
+//		void handleServerNameInServ(VirtualServer& v_serv, int *which_one_exist);
+		void handleRootInServ(VirtualServer& v_serv, int *which_one_exist);
+		void handleIndexInServ(VirtualServer& v_serv, int *which_one_exist);
+		void handleReturnInServ(VirtualServer& v_serv, int *which_one_exist);
+		void handleMethodInServ(VirtualServer& v_serv, int *which_one_exist);
+		void handleAutoindexInServ(VirtualServer& v_serv, int *which_one_exist);
+		void handleUploadPathInServ(VirtualServer& v_serv);
+//		void handleUploadPathInServ(VirtualServer& v_serv, int *which_one_exist);
+		void handleMaxBodySizeInServ(VirtualServer& v_serv, int *which_one_exist);
+//		void handleLocationInServ(VirtualServer& v_serv, int *which_one_exist);
+		void handleErrorPageInServ(VirtualServer& v_serv);
+		void handleCgiExtInServ(VirtualServer& v_serv, int *which_one_exist);
+
+		void handleRootInLoc(Location& location, int *which_one_exist);
+		void handleIndexInLoc(Location& location, int *which_one_exist);
+		void handleReturnInLoc(Location& location, int *which_one_exist);
+		void handleMethodInLoc(Location& location, int *which_one_exist);
+		void handleAutoindexInLoc(Location& location, int *which_one_exist);
+		void handleUploadPathInLoc(Location& location, int *which_one_exist);
+		void handleMaxBodySizeInLoc(Location& location, int *which_one_exist);
+		void handleAliasInLoc(Location& location, int *which_one_exist);
+//		void handleLocationInLoc(Location& location, int *which_one_exist);
+		void handleErrorPageInLoc(Location& location, int *which_one_exist);
+		void handleCgiExtInLoc(Location& location, int *which_one_exist);
+		class SyntaxException: public std::exception {
+			public:
+				explicit SyntaxException(const std::string& what_arg);
+				~SyntaxException() throw();
+				virtual const char* what() const throw();
+			private:
+				std::string msg;
+		};
+		class DupulicateException: public std::exception {
+			public:
+				explicit DupulicateException(const std::string& what_arg);
+				~DupulicateException() throw();
+				virtual const char* what() const throw();
+			private:
+				std::string msg;
+		};
+		class ConfigValueException: public std::exception {
+			public:
+				explicit ConfigValueException(const std::string& what_arg);
+				~ConfigValueException() throw();
+				virtual const char* what() const throw();
+			private:
+				std::string msg;
+		};
+};
+
+const std::string readConfFile(const std::string& file_name);
+#endif
---
+++ srcs/conf/VirtualServer.hpp
@@ -0,0 +1,81 @@
+#ifndef VIRTUALSERVER_HPP
+#define VIRTUALSERVER_HPP
+
+#include <vector>
+#include <map>
+#include <string>
+#include "Location.hpp"
+
+class VirtualServer {
+	public:
+		VirtualServer();
+		VirtualServer(const VirtualServer& src);
+		VirtualServer& operator=(const VirtualServer& rhs);
+		~VirtualServer();
+
+		void parseVserv();
+		void setListen(int listen);
+		void setServerName(std::vector<std::string> server_name);
+		void setLocation(Location location);
+		void setRoot(std::string root);
+		std::vector<Location> getLocations() const;
+        std::vector<int> getListen() const;
+        std::vector<std::string> getServerNames() const;
+		std::string getRoot() const;
+		std::map<std::string, Location> getUri2Location() const;
+		void setUri2location(std::map<std::string, Location> uri2location);
+
+
+        void setMethods(std::vector<std::string> methods);
+		void setIsAutoindex(bool autoindex);
+		void setUploadPath(std::string upload_path);
+		void setIndex(std::vector<std::string> index);
+		void setMaxBodySize(size_t max_body_size);
+		void setCgiPath(std::string cgi_path);
+		void setReturn(std::string ret);
+		void setDepth(int depth);
+		void setAlias(std::string alias);
+		void setErrorPages(std::vector<std::string> tokens);
+		void setCgiExt(std::vector<std::string> tokens);
+
+		std::vector<std::string> get_methods() const;
+		bool getIsAutoindex() const;
+		std::string getUploadPath() const;
+        std::vector<std::string> getIndex() const;
+		size_t getMaxBodySize() const;
+		std::string getCgiPath() const;
+		std::string getReturn() const;
+		std::string getErrorPage(int status_code) const;
+		std::map<int, std::string> getErrorPages() const;
+		int getDepth() const;
+		std::string getAlias() const;
+		std::vector<std::string> getCgiExt() const;
+
+        void appendIndex(std::vector<std::string> elems);
+        void appendCgiExt(std::vector<std::string> elems);
+	private:
+        std::vector<int> listen;
+        std::vector<std::string> server_names;
+		std::string root;
+		std::vector<Location> locations;
+		std::string path;
+		std::string error_page;
+		std::map<std::string, Location> uri2location;
+
+        int whichOneExist;
+
+        std::vector<std::string> index;
+		std::string ret;
+		std::vector<std::string> methods;
+		std::string upload_path;
+		std::string cgi_path;
+		std::map<int, std::string > error_pages;
+		int depth;
+		std::string alias;
+		bool autoindex;
+		size_t max_body_size;
+		std::vector<std::string> cgi_ext;
+};
+
+std::ostream& operator <<(std::ostream& stream, const VirtualServer& obj);
+#endif
